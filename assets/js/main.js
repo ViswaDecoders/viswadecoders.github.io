cf7413437066128ecf46e519bf1a883e695d8b83
Viswanath <68096704+ViswaDecoders@users.noreply.github.com>
2022-07-15 11:40:12 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,26 @@
 */
 (function() {
   "use strict";
+  /**
+   * Visitor  count
+   */
+   window.onload = function(){
+    var n = localStorage.getItem('on_load_counter');
+    if (n === null) {
+      n = 0;
+    }
+    n++;
 
+    localStorage.setItem("on_load_counter", n);
+    document.getElementById('CounterVisitor').innerHTML = n;
+    
+    // nums = n.toString().split('').map(Number);
+    // document.getElementById('CounterVisitor').innerHTML = '';
+    // for (var i of nums) {
+    //   document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
+    // }
+  }
+  
   /**
    * Easy selector helper function
    */
